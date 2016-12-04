4a26e307adccc6b03fa451f2b1e0f9652afacf7a
Allan Lasser <lasser.allan@gmail.com>
2016-12-04 13:53:26 -0500

    supports redux dev tools

--- src/client/store.js
+++ src/client/store.js
@@ -1,9 +1,18 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './reducers';
 
+const isDev = process.env.NODE_ENV !== 'production';
+
 function configureStore(initialState) {
-  return createStore(rootReducer, initialState);
+  if (isDev) {
+    return createStore(rootReducer, initialState, composeWithDevTools(
+      applyMiddleware(thunk),
+    ))
+  }
+  return createStore(rootReducer, initialState, applyMiddleware(thunk));
 }
 
 export default configureStore;

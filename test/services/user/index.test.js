86779673a9949c4f135cb606b00e84aa20b077e5
Allan Lasser <lasser.allan@gmail.com>
2016-12-05 16:16:35 -0500

    fixes the relative paths in feathers tests

--- test/services/user/index.test.js
+++ test/services/user/index.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const assert = require('assert');
-const app = require('../../../src/app');
+const app = require('../../../src/server/app');
 
 describe('user service', function() {
   it('registered the users service', () => {

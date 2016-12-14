5ae91e3c73d5a3dfe7d4a942c1ae87dd6ddec1db
Allan Lasser <lasser.allan@gmail.com>
2016-12-14 12:12:46 -0500

    ignores content and component directories when watching for server updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,8 @@
 import React, { PropTypes } from 'react';
 
 const App = ({children}) => (
-  <div id="app">
-    <h1>
-      <span className="rocket">🚀</span>
-      <span className="success">Ready for blastoff!</span>
-    </h1>
+  <div id="launchpad">
+    <h1>Ready for liftoff!</h1>
     {children}
   </div>
 );

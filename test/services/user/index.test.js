3f5dabaeedf5e1b42bcf42c385bf127c13f054d1
Allan Lasser <lasser.allan@gmail.com>
2016-12-15 11:55:58 -0500

    fixes test imports

--- test/services/user/index.test.js
+++ test/services/user/index.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const assert = require('assert');
-const app = require('../../../src/server/app');
+const app = require('../../../src/app');
 
 describe('user service', function() {
   it('registered the users service', () => {

a047d57b7bc51ce1ad92c243e12162495e1944f7
Allan Lasser <lasser.allan@gmail.com>
2016-12-19 18:32:21 -0500

    adds tests for component behaviors and stores

--- test/components/Home.js
+++ test/components/Home.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { expect } from 'chai';
 
+import configureStore from '../../src/store';
 import Home from '../../src/components/Home';
 
-describe('<Home />', () => {
-  let component;
-  beforeEach(() => {
-    component = shallow(<Home />);
+describe('<Home /> component', () => {
+  describe('rendering', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = shallow(<Home />);
+    });
+    it('renders as a <div>', () => {
+      expect(wrapper.is('div')).to.be.true;
+    });
+    it('presents an image', () => {
+      expect(wrapper.find('img')).to.have.lengthOf(1);
+      expect(wrapper.find('img').get(0).props.src).to.equal('/launchpad.png');
+    });
+    it('presents a link to /about', () => {
+      expect(wrapper.find('Link')).to.have.lengthOf(1);
+      expect(wrapper.find('Link').get(0).props.to).to.equal('/about');
+    });
   });
-  it('renders as a <div>', () => {
-    expect(component.is('div')).to.be.true;
+  describe('behavior', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(<Home />);
+    });
+    it('works!', () => {
+      expect(wrapper).to.be.an('object');
+    });
   });
-  it('presents an image', () => {
-    expect(component.find('img')).to.have.lengthOf(1);
-    expect(component.find('img').get(0).props.src).to.equal('/launchpad.png');
+  describe('state', () => {
+    let store;
+    beforeEach(() => {
+      store = configureStore({});
+    });
+    it('works!', () => {
+      expect(store).to.be.an('object');
+    });
   });
 });

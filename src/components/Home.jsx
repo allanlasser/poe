3cead90416ff68a8005cb1ddc8ae99f4ac41262f
Allan Lasser <lasser.allan@gmail.com>
2016-12-29 23:22:01 -0500

    makes it fun
    I had fun making this part

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,11 +6,13 @@ import LaunchChecklist from '../containers/LaunchChecklist';
 const Home = () => (
   <div className="fullpage cover bg-pink pv4" id="home">
     <div className="w-100 mw6 ph3 b">
-      <header className="flex justify-between items-center mb4 f5">
-        <p className="ma0">Launchpad v1.0.0</p>
-        <p className="ma0"><Link className="mr0 pa2 bg-black" to="/about">About &rarr;</Link></p>
+      <header className="flex justify-between items-center mb2 f5">
+        <p className="ma0"><a href="https://github.com/worb/launchpad" target="_blank" rel="noopener noreferrer" className="white">Launchpad v1.0.0</a></p>
       </header>
-      <img src="/launchpad.png" className="mb4 db" />
+      <Link className="pink db mb4 white relative" to="/about" id="launch-button">
+        <p className="absolute tc">&larr; LAUNCH</p>
+        <img src="/buttons.png" title="Ready when you are, commander." className="db dither" />
+      </Link>
       <h1 className="mt0 mb4 tl f1">Ready for liftoff.</h1>
       <div className="mb1 bg-white-80 ba b--black-10 black launchChecklist">
         <p className="ma0 b pa3 bb b--black-10">Launch Checklist</p>

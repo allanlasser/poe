11db375c23e0cf5fe677246884cad7874ca9f2dd
Allan Lasser <lasser.allan@gmail.com>
2017-01-04 16:43:14 -0700

    fixes tests

--- test/components/Home.js
+++ test/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { shallow, mount } from 'enzyme';
 import { expect } from 'chai';
 
 import configureStore from '../../src/store';
 import Home from '../../src/components/Home';
+import LaunchChecklist from '../../src/containers/LaunchChecklist';
 
 describe('<Home /> component', () => {
   describe('rendering', () => {
@@ -16,17 +18,23 @@ describe('<Home /> component', () => {
     });
     it('presents an image', () => {
       expect(wrapper.find('img')).to.have.lengthOf(1);
-      expect(wrapper.find('img').get(0).props.src).to.equal('/launchpad.png');
+      expect(wrapper.find('img').get(0).props.src).to.equal('/buttons.png');
     });
     it('presents a link to /about', () => {
       expect(wrapper.find('Link')).to.have.lengthOf(1);
       expect(wrapper.find('Link').get(0).props.to).to.equal('/about');
     });
+    it('renders a <LaunchChecklist />', () => {
+      expect(wrapper.find(LaunchChecklist)).to.have.lengthOf(1);
+    });
   });
   describe('behavior', () => {
     let wrapper;
     beforeEach(() => {
-      wrapper = mount(<Home />);
+      wrapper = mount(
+        <Provider store={configureStore({})}>
+          <Home />
+        </Provider>);
     });
     it('works!', () => {
       expect(wrapper).to.be.an('object');

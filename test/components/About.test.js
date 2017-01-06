2c97227cbddb6ed4c13d9af6f4bd6a2dbffbc7eb
Allan Lasser <lasser.allan@gmail.com>
2017-01-06 09:40:55 -0700

    removes state tests from components
    Components should be stateless.

--- test/components/About.test.js
+++ test/components/About.test.js
@@ -1,43 +1,22 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import { expect } from 'chai';
 
-import configureStore from '../../src/store';
 import About from '../../src/components/About';
 
 describe('<About /> component', () => {
-  describe('rendering', () => {
-    let wrapper;
-    beforeEach(() => {
-      wrapper = shallow(<About />);
-    });
-    it('renders as a <div>', () => {
-      expect(wrapper.is('div')).to.be.true;
-    });
-    it('says "Hello, World"!', () => {
-      expect(wrapper.find('h1').text()).to.equal('Hello, World.');
-    });
-    it('presents a link to /', () => {
-      expect(wrapper.find('Link')).to.have.lengthOf(1);
-      expect(wrapper.find('Link').get(0).props.to).to.equal('/');
-    });
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<About />);
   });
-  describe('behavior', () => {
-    let wrapper;
-    beforeEach(() => {
-      wrapper = mount(<About />);
-    });
-    it('works!', () => {
-      expect(wrapper).to.be.an('object');
-    });
+  it('renders as a <div>', () => {
+    expect(wrapper.is('div')).to.be.true;
   });
-  describe('state', () => {
-    let store;
-    beforeEach(() => {
-      store = configureStore({});
-    });
-    it('works!', () => {
-      expect(store).to.be.an('object');
-    });
+  it('says "Hello, World"!', () => {
+    expect(wrapper.find('h1').text()).to.equal('Hello, World.');
+  });
+  it('presents a link to /', () => {
+    expect(wrapper.find('Link')).to.have.lengthOf(1);
+    expect(wrapper.find('Link').get(0).props.to).to.equal('/');
   });
 });
--- test/components/Home.test.js
+++ test/components/Home.test.js
@@ -1,52 +1,27 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import { expect } from 'chai';
 
-import configureStore from '../../src/store';
 import Home from '../../src/components/Home';
 import LaunchChecklist from '../../src/containers/LaunchChecklist';
 
 describe('<Home /> component', () => {
-  describe('rendering', () => {
-    let wrapper;
-    beforeEach(() => {
-      wrapper = shallow(<Home />);
-    });
-    it('renders as a <div>', () => {
-      expect(wrapper.is('div')).to.be.true;
-    });
-    it('presents an image', () => {
-      expect(wrapper.find('img')).to.have.lengthOf(1);
-      expect(wrapper.find('img').get(0).props.src).to.equal('/buttons.png');
-    });
-    it('presents a link to /about', () => {
-      expect(wrapper.find('Link')).to.have.lengthOf(1);
-      expect(wrapper.find('Link').get(0).props.to).to.equal('/about');
-    });
-    it('renders a <LaunchChecklist />', () => {
-      expect(wrapper.find(LaunchChecklist)).to.have.lengthOf(1);
-    });
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Home />);
   });
-  describe('behavior', () => {
-    let wrapper;
-    beforeEach(() => {
-      wrapper = mount(
-        <Provider store={configureStore({})}>
-          <Home />
-        </Provider>);
-    });
-    it('works!', () => {
-      expect(wrapper).to.be.an('object');
-    });
+  it('renders as a <div>', () => {
+    expect(wrapper.is('div')).to.be.true;
   });
-  describe('state', () => {
-    let store;
-    beforeEach(() => {
-      store = configureStore({});
-    });
-    it('works!', () => {
-      expect(store).to.be.an('object');
-    });
+  it('presents an image', () => {
+    expect(wrapper.find('img')).to.have.lengthOf(1);
+    expect(wrapper.find('img').get(0).props.src).to.equal('/buttons.png');
+  });
+  it('presents a link to /about', () => {
+    expect(wrapper.find('Link')).to.have.lengthOf(1);
+    expect(wrapper.find('Link').get(0).props.to).to.equal('/about');
+  });
+  it('renders a <LaunchChecklist />', () => {
+    expect(wrapper.find(LaunchChecklist)).to.have.lengthOf(1);
   });
 });

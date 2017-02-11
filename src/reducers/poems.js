b76299a3b81a9ab18f47755118e7607d50a38fa9
Allan Lasser <lasser.allan@gmail.com>
2017-02-11 18:09:02 -0500

    adds a reducer for poems

---
+++ src/reducers/poems.js
@@ -0,0 +1,35 @@
+function poem(state = {
+  isFetching: false,
+  poem: {
+    title: null,
+    author: null,
+    lines: [],
+    lineCount: 0
+  }
+}, action) {
+  switch (action.type) {
+    case 'REQUEST_POEM':
+      return Object.assign({}, state, {
+        isFetching: true,
+      });
+    case 'RECEIVE_POEM':
+      return Object.assign({}, state, {
+        isFetching: false,
+        poem: action.poem
+      });
+    default:
+      return state;
+  }
+}
+
+export function poems(state = {}, action) {
+  switch (action.type) {
+    case 'REQUEST_POEM':
+    case 'RECEIVE_POEM':
+      return Object.assign({}, state, {
+        [action.title]: poem([action.title], action)
+      });
+    default:
+      return state;
+  }
+}
